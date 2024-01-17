145e14c143e7a3f9917ea80e45b71fac42653a97
daniilmartynov <daniil.martynov@amo.tech>
2024-01-17 14:41:14 +0800

    Add autoplay

--- components/constructivism.tsx
+++ components/constructivism.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { useState, useCallback, useRef } from 'react'
+import { useState, useEffect, useCallback, useRef } from 'react'
 import Image from 'next/image'
+
+import { cn } from '@/lib/utils'
 
 import pic1 from '@/public/1.webp'
 import pic2 from '@/public/2.webp'
@@ -9,13 +11,15 @@ import pic3 from '@/public/3.webp'
 import pic4 from '@/public/4.webp'
 import pic5 from '@/public/5.webp'
 
-const items = [
-  { src: pic1, alt: 'MEG' },
-  { src: pic2, alt: 'DIE' },
-  { src: pic3, alt: 'HDC' },
-  { src: pic4, alt: 'PENTAGRAM' },
-  { src: pic5, alt: 'TOR' },
-]
+const config = {
+  items: [
+    { src: pic1, alt: 'MEG' },
+    { src: pic2, alt: 'DIE' },
+    { src: pic3, alt: 'HDC' },
+    { src: pic4, alt: 'PENTAGRAM' },
+    { src: pic5, alt: 'TOR' },
+  ],
+}
 
 const WIDTH = 40
 const HEIGHT = 30
@@ -23,7 +27,9 @@ const PERSPECTIVE = 5000
 
 export default function Constructivism() {
   const [degree, setDegree] = useState([0, 0])
+  const [isAutoplay, setIsAutoplay] = useState(false)
 
+  const isDragging = useRef(false)
   const touchStart = useRef({ x: 0, y: 0 })
 
   const handleImageLoad = useCallback(
@@ -51,6 +57,7 @@ export default function Constructivism() {
   const handleTouchStart = useCallback((event: React.TouchEvent) => {
     const touch = event.touches[0]
     touchStart.current = { x: touch.clientX, y: touch.clientY }
+    setIsAutoplay(false)
   }, [])
 
   const handleTouchMove = useCallback(
@@ -65,11 +72,10 @@ export default function Constructivism() {
     [handleMove]
   )
 
-  const isDragging = useRef(false)
-
   const handleMouseDown = useCallback((event: React.MouseEvent) => {
     touchStart.current = { x: event.clientX, y: event.clientY }
     isDragging.current = true
+    setIsAutoplay(false)
   }, [])
 
   const handleMouseMove = useCallback(
@@ -88,9 +94,20 @@ export default function Constructivism() {
     isDragging.current = false
   }, [])
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      const isTouched = Object.values(touchStart.current).some(Boolean)
+      setIsAutoplay(!isTouched)
+    }, 10000)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
+
   return (
     <div
-      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-1000 ease-in-out cursor-move"
+      className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-move"
       style={{
         width: `${WIDTH}vw`,
         height: `${HEIGHT}vh`,
@@ -103,13 +120,16 @@ export default function Constructivism() {
       onTouchStart={handleTouchStart}
     >
       <div
-        className="absolute w-full h-full transition-all duration-1000 ease-in-out"
+        className={cn(
+          'absolute w-full h-full transition-transform duration-500 ease-in-out',
+          isAutoplay && 'animate-autoplay'
+        )}
         style={{
           transform: `rotateX(${degree[0]}deg) rotateY(${degree[1]}deg)`,
           transformStyle: 'preserve-3d',
         }}
       >
-        {items.map((item, index, array) => (
+        {config.items.map((item, index, array) => (
           <div
             key={item.alt}
             className="absolute bg-no-repeat bg-center overflow-hidden border border-solid border-neutral-300"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,19 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      keyframes: {
+        autoplay: {
+          '0%': { transform: 'rotateY(0deg)' },
+          '100%': { transform: 'rotateY(360deg)' },
+        },
+      },
+      animation: {
+        autoplay: 'autoplay 60s linear infinite',
+      },
+    },
+  },
   plugins: [],
 }
 export default config
