12c390eb85365eb7db0fdd68892f6de6b98b1c04
daniilmartynov <daniil.martynov@amo.tech>
2024-01-17 13:57:50 +0800

    Remove logotype

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Constructivism from '@/components/constructivism'
-import Logotype from '@/components/logotype'
 import Window from '@/components/window'
 
 export default function Home() {
   return (
     <main className="h-svh overflow-y-auto">
-      <Logotype />
       <Constructivism />
       <Window />
     </main>
