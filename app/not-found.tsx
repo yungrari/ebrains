bde36793a06702ec11eaa199b1c7217fa7a93fde
daniilmartynov <daniil.martynov@amo.tech>
2024-05-18 21:30:13 +0200

    Update not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,3 @@
 export default function NotFound() {
-  return (
-    <h1 className="fixed top-0 left-0 z-10 p-2 font-mono uppercase text-8xl">
-      404 Not Found
-    </h1>
-  )
+  return <></>
 }
